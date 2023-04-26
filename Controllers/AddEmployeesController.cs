fd38b14d4040fd5995b2b9cc0b52bc0cb37f2dcc
= <nurzhan.aytimov@gmail.com>
2023-04-26 19:11:21 +0600

    EmployeeData
    web project with database connection for adding, deleting and editing employees

---
+++ Controllers/AddEmployeesController.cs
@@ -0,0 +1,75 @@
+﻿using Employee.Data;
+using Employee.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Controllers
+{
+    public class AddEmployeesController : Controller
+    {
+        private readonly EmployeeDbContext employeesDbContext;
+        public AddEmployeesController(EmployeeDbContext employeesDbContext)
+        {
+            this.employeesDbContext = employeesDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var employee = employeesDbContext.Employees.ToList();
+            return View(employee);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        //добавление сотрудника
+        [HttpPost]
+        public IActionResult Add(Employees employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employeesDbContext.Employees.Add(employee);
+                employeesDbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var employee = employeesDbContext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        //метод для редактирования
+        [HttpPost]
+        public IActionResult Edit(Employees employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employeesDbContext.Employees.Update(employee);
+                employeesDbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+        //метод для удаления
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var employee = employeesDbContext.Employees.Find(id);
+            if (employee != null)
+            {
+                employeesDbContext.Employees.Remove(employee);
+                employeesDbContext.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Employees.cs
@@ -0,0 +1,13 @@
+﻿namespace Employee.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+        public DateTime DateBirth { get; set; }
+    }
+}
